673be6ae686afbda65759b168961fe2574c1aa18
Denis Kalinochkin <kalinochkind@gmail.com>
2021-02-14 14:56:09 +0300

    0.2.0 release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='otvetmailru',
-    version='0.1.2',
+    version='0.2.0',
     description='otvet.mail.ru API wrapper',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -37,6 +37,4 @@ setup(
         'dataclasses;python_version<"3.7"',
     ],
     python_requires=">=3.6",
-
-
 )
